57645063977914d2c0f17fb11feed2e81de1a44a
letrungviet2005 <viet.lt.23it@vku.udn.vn>
2024-09-09 23:58:35 +0700

    new

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,19 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import "@fortawesome/fontawesome-free/css/all.min.css";
 import classNames from "classnames/bind";
 import logo from "../../assets/images/logo.png";
 import style from "./Navbar.module.scss";
 import facebook from "../../assets/images/facebook.svg";
 import instagram from "../../assets/images/instagram.svg";
+
 const cx = classNames.bind(style);
+
 function Navbar() {
+  const [menuVisible, setMenuVisible] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuVisible(!menuVisible);
+  };
+
   return (
     <div className={cx("navbar")}>
       <div className={cx("menu")}>
         <div className={cx("logo")}>
           <img src={logo} alt="logo" />
         </div>
-        <div className={cx("menu-list")}>
+        <div className={cx("hamburger")} onClick={toggleMenu}>
+          <i className="fa-solid fa-bars"></i>
+        </div>
+        <div className={cx("menu-list", { "menu-list-slide": menuVisible })}>
           <Link to="/" className={cx("menu-item")}>
             Home
           </Link>
