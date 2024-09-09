711f6d7f0867e6ecf5e5b43a19ddd5e91ead95fa
letrungviet2005 <viet.lt.23it@vku.udn.vn>
2024-09-09 13:43:46 +0700

    new

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "../src/components/Navbar/Navbar.js";
+import Products from "../src/pages/Products/Products.js";
+import Contact from "../src/pages/Contact/Contact.js";
+import Home from "../src/pages/Home/Home.js";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/pages/Products/Products.js
@@ -0,0 +1,9 @@
+function Products() {
+    return (
+        <div>
+            <p>Products</p>
+        </div>
+      );
+}
+
+export default Products;
