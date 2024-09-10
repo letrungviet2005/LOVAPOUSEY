3af0cacf65953a0f89aa21aae5ce7811ae170bd5
letrungviet2005 <viet.lt.23it@vku.udn.vn>
2024-09-10 22:58:17 +0700

    new

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -19,48 +19,51 @@ function Navbar() {
 
   return (
     <div>
-      <div className={cx("navbar")}>
-        <div className={cx("menu")}>
-          <div className={cx("logo")}>
-            <img src={logo} alt="logo" />
-          </div>
-          <div className={cx("hamburger")} onClick={toggleMenu}>
-            <i className="fa-solid fa-bars"></i>
-          </div>
-          <div className={cx("menu-list")}>
-            <Link to="/" className={cx("menu-item")}>
-              Home
-            </Link>
-            <Link to="/portfolio" className={cx("menu-item")}>
-              Portfolio
-            </Link>
-            <Link to="/Contact" className={cx("menu-item")}>
-              Contact
-            </Link>
-            <img src={facebook} alt="facebook" />
-            <img src={instagram} alt="instagram" />
-          </div>
-        </div>
-      </div>
-      <div className={cx("menu-mobile", { hidden: !menuVisible })}>
-        <div className={cx("menu-list-mobile")}>
-          <Link to="/" className={cx("menu-item-mobile")}>
-            Home
-          </Link>
-          <Link to="/portfolio" className={cx("menu-item-mobile")}>
-            Portfolio
-          </Link>
-          <Link to="/contact" className={cx("menu-item-mobile")}>
-            Contact
-          </Link>
-          <div style={{ display: "flex" }}>
-            <div className={cx("menu-social-mobile")}>
+      <div className={cx("container")}>
+        <div className={cx("navbar")}>
+          <div className={cx("menu")}>
+            <div className={cx("logo")}>
+              <img src={logo} alt="logo" />
+            </div>
+            <div className={cx("hamburger")} onClick={toggleMenu}>
+              <i className="fa-solid fa-bars"></i>
+            </div>
+            <div className={cx("menu-list")}>
+              <Link to="/" className={cx("menu-item")}>
+                Home
+              </Link>
+              <Link to="/portfolio" className={cx("menu-item")}>
+                Portfolio
+              </Link>
+              <Link to="/Contact" className={cx("menu-item")}>
+                Contact
+              </Link>
               <img src={facebook} alt="facebook" />
               <img src={instagram} alt="instagram" />
             </div>
           </div>
         </div>
+        <div className={cx("menu-mobile", { hidden: !menuVisible })}>
+          <div className={cx("menu-list-mobile")}>
+            <Link to="/" className={cx("menu-item-mobile")}>
+              Home
+            </Link>
+            <Link to="/portfolio" className={cx("menu-item-mobile")}>
+              Portfolio
+            </Link>
+            <Link to="/contact" className={cx("menu-item-mobile")}>
+              Contact
+            </Link>
+            <div style={{ display: "flex" }}>
+              <div className={cx("menu-social-mobile")}>
+                <img src={facebook} alt="facebook" />
+                <img src={instagram} alt="instagram" />
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
+      <div className={cx("space")}></div>
     </div>
   );
 }
